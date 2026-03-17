2ee7e3bf7263e8c10c85a4b9382771568759fa13
hcketjow <hcketjow@gmail.com>
2026-03-17 14:56:23 +0100

    Structure of a project

---
+++ Projector.cs
@@ -0,0 +1,6 @@
+﻿namespace RentEquipment;
+
+public class Projector
+{
+    
+}
---
+++ Rent.cs
@@ -0,0 +1,25 @@
+﻿namespace RentEquipment;
+
+public class Rent {
+    private DateTime RentDate;
+    private DateTime ReturnDate;
+    private DateTime ActualReturnDate;
+    private double RentPrice;
+    private double RentPenalty;
+
+    public Rent(
+        DateTime rentDate, 
+        DateTime returnDate,
+        DateTime actualReturnDate,
+        double rentPrice, 
+        double rentPenalty
+    ) {
+        RentDate = rentDate;
+        ReturnDate = returnDate;
+        ActualReturnDate = actualReturnDate;
+        RentPrice = rentPrice;
+        RentPenalty = rentPenalty;
+    }
+    
+    
+}
